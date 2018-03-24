6a68a18e6b1b9cdc993da21a717827d6d3611f6a
Xoma163 <Xoma163rus@gmail.com>
2018-03-24 12:29:50 +0400

    Initical commit #2

---
+++ GemCalculator/GemCalc/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace GemCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxGuild = new System.Windows.Forms.TextBox();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.textBoxMaxIter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxResults = new System.Windows.Forms.TextBox();
+            this.menuForm1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.menuForm1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 447);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(11, 34);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(143, 281);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBoxGuild
+            // 
+            this.textBoxGuild.Location = new System.Drawing.Point(54, 399);
+            this.textBoxGuild.Name = "textBoxGuild";
+            this.textBoxGuild.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGuild.TabIndex = 3;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(54, 321);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMin.TabIndex = 4;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(54, 347);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMax.TabIndex = 5;
+            // 
+            // textBoxMaxIter
+            // 
+            this.textBoxMaxIter.Location = new System.Drawing.Point(54, 373);
+            this.textBoxMaxIter.Name = "textBoxMaxIter";
+            this.textBoxMaxIter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxMaxIter.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Max";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Max op.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 402);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Guild";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 428);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Results";
+            // 
+            // textBoxResults
+            // 
+            this.textBoxResults.Location = new System.Drawing.Point(54, 425);
+            this.textBoxResults.Name = "textBoxResults";
+            this.textBoxResults.Size = new System.Drawing.Size(100, 20);
+            this.textBoxResults.TabIndex = 11;
+            // 
+            // menuForm1
+            // 
+            this.menuForm1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuForm1.Location = new System.Drawing.Point(0, 0);
+            this.menuForm1.Name = "menuForm1";
+            this.menuForm1.Size = new System.Drawing.Size(166, 24);
+            this.menuForm1.TabIndex = 13;
+            this.menuForm1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(184, 34);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(10, 411);
+            this.dgv.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(166, 483);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.textBoxMaxIter);
+            this.Controls.Add(this.textBoxGuild);
+            this.Controls.Add(this.textBoxResults);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuForm1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuForm1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gem Calculator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            this.menuForm1.ResumeLayout(false);
+            this.menuForm1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBoxMin;
+        private System.Windows.Forms.TextBox textBoxMax;
+        private System.Windows.Forms.TextBox textBoxMaxIter;
+        private System.Windows.Forms.TextBox textBoxGuild;
+        private System.Windows.Forms.TextBox textBoxResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MenuStrip menuForm1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
+
---
+++ GemCalculator/GemCalc/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GemCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Кол-во общих возможных исходов
+        int good_Combinations = 0;
+        //Массив общих возможных исходов
+        string[] good_Combinations_arr;
+        //Кол-во благоприятных исходовв
+        int good_Combinations_minmax = 0;
+        //Массив благоприятных исходов
+        int[,] all_Combinations;
+
+        //Список камней
+        int[] gems;
+        //Мин и макс значения для выборки
+        int max;
+        int min;
+        //Макс итераций
+        int max_iter;
+        //Макс результатов
+        int max_res;
+        //Кол-во камней
+        int gems_count;
+        //Бонус гильды
+        int guild_bonus;
+
+        int summ=0;
+        bool iWasHere = true;
+
+        //Массив для подсчёта количества благоприятных исходов
+        int[] tempMas;
+
+        //Добиваем до нужного количества нулей
+        private string AddString0(int count)
+        {
+            string text_0 = "";
+            for (int i = 0; i < count; i++)
+                text_0 += "0";
+            return text_0;
+        }
+        //проверка на нечётность и >3
+        private bool IsOdd(int n)
+
+        {
+            if ((n % 2 == 1) && (n > 1))
+                return true;
+            else
+                return false;
+        }
+        //Возвращает количество единиц из двоичного представления
+        private int Count_1(string s)
+        {
+            return s.Split(new string[] { "1" }, StringSplitOptions.None).Count() - 1;
+        }
+
+        //жмяк по кнопке. Точка входа
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            
+            int count = dgv.Columns.Count;
+            for (int i = 0; i < count; i++)
+            {
+                dgv.Columns.RemoveAt(dgv.Columns.Count-1);
+            }
+
+            if (!iWasHere)
+            {
+                this.Width -= summ + 35;
+                dgv.Width -= summ;
+            }
+            iWasHere = false;
+            // dgv.Rows.Clear();
+
+
+            gems = new int[richTextBox1.Lines.Length];
+            //Костыль для считывания списка камней с richTextBox
+            string[] gems_string = new string[richTextBox1.Lines.Length];
+            max = Convert.ToInt32(textBoxMax.Text);
+            min = Convert.ToInt32(textBoxMin.Text);
+            max_iter = Convert.ToInt32(textBoxMaxIter.Text);
+            max_res = Convert.ToInt32(textBoxResults.Text);
+            gems_count = gems.Length;
+            guild_bonus = Convert.ToInt32(textBoxGuild.Text);
+
+
+            //Считывание камней с richTextBox
+            gems_string = richTextBox1.Text.Split();
+            for (int i = 0; i < gems_count; i++)
+                Int32.TryParse(gems_string[i],out gems[i]);
+            //gems[i] =  Convert.ToInt32(gems_string[i]);
+
+
+            //Узнаю общее число возможных крафтов
+            good_Combinations_arr = new string[good_Combinations];
+            good_Combinations = 0;
+            for (int i = 0; i < Math.Pow(2, gems_count); i++)
+            {
+                if (IsOdd(Count_1(Convert.ToString(i, 2))))
+                    good_Combinations++;
+            }
+
+
+            //Записываю всевозможные крафты в один массив good_Combinations_arr
+            good_Combinations_arr = new string[good_Combinations];
+            good_Combinations = 0;
+            string binary = "";
+            for (int i = 0; i < Math.Pow(2, gems_count); i++)
+            {
+                binary = Convert.ToString(i, 2);
+                binary = AddString0(gems_count - binary.Length) + binary;
+
+                if (IsOdd(Count_1(binary)))
+                {
+                    good_Combinations_arr[good_Combinations] = binary;
+                    good_Combinations++;
+                }
+            }
+
+
+            //Считаю количество благоприятных, т.е. [min;max]
+            good_Combinations_minmax = 0;
+            tempMas = new int[gems_count + 2];
+
+            for (int k = 0; k < good_Combinations; k++)
+            {
+                Array.Clear(tempMas, 0, tempMas.Length);
+                for (int i = 0; i < gems_count; i++)
+                {
+                    tempMas[i] = gems[i] * (int)Char.GetNumericValue(good_Combinations_arr[k][i]);
+                    tempMas[gems_count] += tempMas[i];
+                    if (tempMas[i] != 0)
+                        tempMas[gems_count + 1] += 1;
+                }
+                tempMas[gems_count + 1] = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(tempMas[gems_count + 1]) / 2D - 1));
+                tempMas[gems_count] += tempMas[gems_count + 1] * guild_bonus;
+                if (tempMas[gems_count] >= min && tempMas[gems_count] <= max)
+                    good_Combinations_minmax++;
+            }
+
+
+            //Записываю в массив all_Combinations все благоприятные, т.е. [min;max]
+            all_Combinations = new int[gems_count + 2, good_Combinations_minmax];
+            int kk = 0;
+
+            for (int k = 0; k < good_Combinations; k++)
+            {
+                Array.Clear(tempMas, 0, tempMas.Length);
+
+                for (int i = 0; i < gems_count; i++)
+                {
+                    tempMas[i] = gems[i] * (int)Char.GetNumericValue(good_Combinations_arr[k][i]);
+                    tempMas[gems_count] += tempMas[i];
+                    if (tempMas[i] != 0)
+                        tempMas[gems_count + 1] += 1;
+                }
+                tempMas[gems_count + 1] = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(tempMas[gems_count + 1]) / 2D - 1));
+                tempMas[gems_count] += tempMas[gems_count + 1] * guild_bonus;
+                if (tempMas[gems_count] >= min && tempMas[gems_count] <= max)
+                {
+                    for (int i = 0; i < gems_count; i++)
+                    {
+                        all_Combinations[i, kk] = gems[i] * (int)Char.GetNumericValue(good_Combinations_arr[k][i]);
+                        all_Combinations[gems_count, kk] += all_Combinations[i, kk];
+                        if (all_Combinations[i, kk] != 0)
+                            all_Combinations[gems_count + 1, kk] += 1;
+                    }
+                    all_Combinations[gems_count + 1, kk] = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(all_Combinations[gems_count + 1, kk]) / 2D - 1));
+                    all_Combinations[gems_count, kk] += all_Combinations[gems_count + 1, kk] * guild_bonus;
+
+                    kk++;
+                }
+            }
+
+
+            //Сортирую полученный массив благоприятных условий
+            for (int i = 0; i < good_Combinations_minmax; i++)
+                for (int k = 0; k < good_Combinations_minmax - i - 1; k++)
+                    if (all_Combinations[gems_count, k] < all_Combinations[gems_count, k + 1])
+                        for (int j = 0; j < gems_count + 2; j++)
+                        {
+                            int temp = all_Combinations[j, k];
+                            all_Combinations[j, k] = all_Combinations[j, k + 1];
+                            all_Combinations[j, k + 1] = temp;
+                        }
+
+            //Записываю полученный массив в dgv
+            dgv.Columns.Add("#", "#");
+            dgv.Columns["#"].Width = 14+ Convert.ToString(good_Combinations_minmax).Length*5;
+            for(int i=0;i<gems_count;i++)
+            { 
+                dgv.Columns.Add(Convert.ToString(i), "" + Convert.ToString(i+1));
+                dgv.Columns[i+1].Width = 45;
+            }
+            dgv.Columns.Add(Convert.ToString(gems_count), "Sum");
+            dgv.Columns[Convert.ToString(gems_count)].Width = 45;
+            dgv.Columns.Add(Convert.ToString(gems_count + 1), "Iter");
+            dgv.Columns[Convert.ToString(gems_count + 1)].Width = 35;
+            int num = 0;
+
+            for (int k = 0; k < good_Combinations_minmax; k++)
+            {
+                if (max_iter == -1 || all_Combinations[gems_count + 1, k] <= max_iter)
+                {
+                    dgv.Rows.Add();
+                    if (max_res == -1)
+                        dgv.Rows[num].Cells[0].Value = AddString0(Convert.ToString(good_Combinations_minmax).Length - Convert.ToString(num + 1).Length) + (Convert.ToString(num + 1));
+                    else
+                        dgv.Rows[num].Cells[0].Value = AddString0(Convert.ToString(max_res).Length - Convert.ToString(num + 1).Length) + (Convert.ToString(num + 1));
+                    for (int i = 0; i < gems.Count() + 2; i++)
+                    {
+                        if (i != gems_count)
+                            dgv.Rows[num].Cells[Convert.ToString(i)].Value = Convert.ToString(all_Combinations[i, k]);
+                        else
+                            dgv.Rows[num].Cells[Convert.ToString(i)].Value = AddString0(Convert.ToString(max).Length - Convert.ToString(all_Combinations[i, k]).Length) + Convert.ToString(all_Combinations[i, k]);
+                    }
+                    num++;
+                }
+                if (num == max_res)
+                    break;
+            }
+            summ = dgv.Columns["#"].Width+ gems_count*45+ dgv.Columns[Convert.ToString(gems_count)].Width+ dgv.Columns[Convert.ToString(gems_count + 1)].Width+50;
+            this.Width += summ+35;
+            dgv.Width += summ;
+
+        }       
+
+        //Методы загрузки/выгрузки
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LogToFile();
+        }
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            StreamReader sr = new StreamReader("Gems.txt");
+            int count = Convert.ToInt32(sr.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                if (i != count - 1)
+                    richTextBox1.AppendText(sr.ReadLine() + Environment.NewLine);
+                else
+                    richTextBox1.AppendText(sr.ReadLine());
+            }
+
+            textBoxMin.Text = sr.ReadLine();
+            textBoxMax.Text = sr.ReadLine();
+            textBoxMaxIter.Text = sr.ReadLine();
+            textBoxGuild.Text = sr.ReadLine();
+            textBoxResults.Text = sr.ReadLine();
+
+            sr.Close();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LogToFile();
+        }
+        private void LogToFile()
+        {
+            StreamWriter sw = new StreamWriter("Gems.txt");
+            sw.WriteLine(richTextBox1.Lines.Length);
+
+            for (int i = 0; i < richTextBox1.Lines.Length; i++)
+                sw.WriteLine(richTextBox1.Lines[i]);
+
+            sw.WriteLine(textBoxMin.Text);
+            sw.WriteLine(textBoxMax.Text);
+            sw.WriteLine(textBoxMaxIter.Text);
+            sw.WriteLine(textBoxGuild.Text);
+            sw.WriteLine(textBoxResults.Text);
+
+            sw.Close();
+        }
+
+    }
+}
